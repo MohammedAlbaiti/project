271528a6af34bb951c71047ea94f725b28d96db6
MohammedAlbaiti <t1234rtr@hotmail.com>
2024-11-04 21:07:15 +0300

    MovingObjects

---
+++ MainClasses/src/MovingObjects.java
@@ -0,0 +1,16 @@
+public abstract class MovingObjects implements GeneralRules{
+    protected double objectSpeed;
+    protected double objectDimension;
+
+    public double getObjectSpeed(){
+
+    }
+    public void setObjectSpeed(objectSpeed:double){
+
+    }
+    public double getObjectDimension(){}
+    public void setObjectDimension(objectDimension:double){}
+    public void move(){}
+    public void stop(){}
+    
+}
