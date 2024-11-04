810ce3ba70da114df056f74b5effadd7bbd3b6ff
abdulrhman-gmd <abdrh220@gmail.com>
2024-11-04 21:45:51 +0300

    Vehicle update 0

---
+++ MainClasses/src/Vehicle.java
@@ -0,0 +1,36 @@
+public class Vehicle extends MovingObjects{
+    String driverStyle;
+
+    public Vehicle(Road road, String driverStyle){
+        this.driverStyle = driverStyle
+    }
+
+    public String getDriverStyle(){
+        return this.driverStyle;
+    }
+
+    public void setDriverStyle(String driverStyle){
+        this.driverStyle = driverStyle;
+    }
+
+    public double getObjectSpeed(){
+        return super.objectSpeed;
+    }
+
+    public void setObjectSpeed(double objectSpeed){
+        super.objectSpeed = objectSpeed;
+    }
+
+    public double getObjectDimension(){
+        return super.objectDimension;
+    }
+
+    public void setObjectDimension(double objectDimension){
+        super.objectDimension = objectDimension;
+    }
+
+    public void move(){}
+
+    public void stop(){}
+
+}
