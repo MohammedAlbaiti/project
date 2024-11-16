49d53a42837895185fa1a799556c990ba5471a0e
MohammedAlbaiti <t1234rtr@hotmail.com>
2024-11-17 01:02:23 +0300

    new

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+// import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
