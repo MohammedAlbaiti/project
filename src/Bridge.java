c6630b122f679abc952f8db62cea920dfa718049
SaudAsiri <asiri.saud@outlook.com>
2024-11-17 00:04:46 +0300

    Updated Bridge.java
    added methods on Bridge.java
    Commented unneeded methods
    made a modification on the path so the code works on my machine.

--- src/Bridge.java
+++ src/Bridge.java
@@ -1,6 +1,6 @@
 public class Bridge implements GeneralRules {
     private boolean bridgeStatus;
-    private double objectDimension;
+    // private double objectDimension;
     public Bridge(boolean bridgeStatus) {
         this.bridgeStatus = bridgeStatus;
     }
@@ -21,13 +21,34 @@ public class Bridge implements GeneralRules {
         this.bridgeStatus = false;
     }
 
+    // TODO: either add these methods in the parent class or just ignore them and continue with the added methods below
+    // @Override
+    // public double getObjectDimension() {
+    //     return this.objectDimension;
+    // }
+
+    // @Override
+    // public void setObjectDimension(double objectDimension) {
+    //     this.objectDimension = objectDimension;
+    // }
+
     @Override
-    public double getObjectDimension() {
-        return this.objectDimension;
+    public double getObjectWidth() {
+        throw new UnsupportedOperationException("Unimplemented method 'getObjectWidth'");
     }
 
     @Override
-    public void setObjectDimension(double objectDimension) {
-        this.objectDimension = objectDimension;
+    public double getObjectHeight() {
+        throw new UnsupportedOperationException("Unimplemented method 'getObjectHeight'");
+    }
+
+    @Override
+    public void setObjectHeight(double objectHeight) {
+        throw new UnsupportedOperationException("Unimplemented method 'setObjectHeight'");
+    }
+
+    @Override
+    public void setObjectWidth(double objectWidth) {
+        throw new UnsupportedOperationException("Unimplemented method 'setObjectWidth'");
     }
 }
