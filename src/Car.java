a77f4db11eb6af3f1d42c9035690ff1ffdbe4daf
MohammedAlbaiti <t1234rtr@hotmail.com>
2024-11-29 12:04:56 +0300

    edit somethings

--- src/Car.java
+++ src/Car.java
@@ -10,8 +10,8 @@ public class Car extends Vehicle {
     private String carType;
     private static int counter = 1;
     private static final SecureRandom random = new SecureRandom();
-    private ArrayList<String> carTypes = new ArrayList<>(Arrays.asList("car", "car2", "car3", "car4", "car5","car6", "car7","car8","car9","car10","car11","car12","car13","car14"));
-    private static ArrayList<Image> carImages = new ArrayList<>(Arrays.asList(new Image("file:src/resources/car.png"),new Image("file:src/resources/car10.png"),new Image("file:src/resources/car2.png"),new Image("file:src/resources/car3.png"),new Image("file:src/resources/car4.png"),new Image("file:src/resources/car5.png"),new Image("file:src/resources/car6.png"),new Image("file:src/resources/car7.png"),new Image("file:src/resources/car8.png"),new Image("file:src/resources/car9.png"),new Image("file:src/resources/car11.png"),new Image("file:src/resources/car12.png"),new Image("file:src/resources/car13.png"),new Image("file:src/resources/car14.png")));
+    private static final ArrayList<String> carTypes = new ArrayList<>(Arrays.asList("car", "car2", "car3", "car4", "car5","car6", "car7","car8","car9","car10","car11","car12","car13","car14"));
+    private static final ArrayList<Image> carImages = new ArrayList<>(Arrays.asList(new Image("file:src/resources/car.png"),new Image("file:src/resources/car10.png"),new Image("file:src/resources/car2.png"),new Image("file:src/resources/car3.png"),new Image("file:src/resources/car4.png"),new Image("file:src/resources/car5.png"),new Image("file:src/resources/car6.png"),new Image("file:src/resources/car7.png"),new Image("file:src/resources/car8.png"),new Image("file:src/resources/car9.png"),new Image("file:src/resources/car11.png"),new Image("file:src/resources/car12.png"),new Image("file:src/resources/car13.png"),new Image("file:src/resources/car14.png")));
     private Image objectImage;
 
 
--- src/Truck.java
+++ src/Truck.java
@@ -9,7 +9,7 @@ public class Truck extends Vehicle {
     private String truckType;
     private static int counter = 1;
     private static final ArrayList<String> TRUCK_TYPES = new ArrayList<>(Arrays.asList("truck", "truck2","truck3","truck4","truck5","truck6"));
-    private static ArrayList<Image> truckImages = new ArrayList<>(Arrays.asList(new Image("file:src/resources/truck.png"),new Image("file:src/resources/truck2.png"),new Image("file:src/resources/truck3.png"),new Image("file:src/resources/truck4.png"),new Image("file:src/resources/truck5.png"),new Image("file:src/resources/truck6.png")));
+    private static final ArrayList<Image> truckImages = new ArrayList<>(Arrays.asList(new Image("file:src/resources/truck.png"),new Image("file:src/resources/truck2.png"),new Image("file:src/resources/truck3.png"),new Image("file:src/resources/truck4.png"),new Image("file:src/resources/truck5.png"),new Image("file:src/resources/truck6.png")));
     private static final SecureRandom random = new SecureRandom();
     // private ImageView truckView;
     // private static final double DEFAULT_TRUCK_SPEED = 2;
