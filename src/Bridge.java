2678d024793777bea18961ca2b2837af83fcd4ee
HussamShaikh <hussam.t.shaikh@gmail.com>
2024-12-08 17:22:24 +0300

    Update Bridge.java
    b

--- src/Bridge.java
+++ src/Bridge.java
@@ -1,6 +1,8 @@
 public class Bridge implements GeneralRules {
     private boolean bridgeStatus;
-    private double objectDimension;
+    // private double objectDimension;
+    private double objectWidth;
+    private double objectHeight=480;
     public Bridge(boolean bridgeStatus) {
         this.bridgeStatus = bridgeStatus;
     }
@@ -15,19 +17,37 @@ public class Bridge implements GeneralRules {
 
     public void enableBridge() {
         this.bridgeStatus = true;
+        
     }
 
     public void disableBridge() {
         this.bridgeStatus = false;
+        
+    }
+    @Override
+    public void setObjectHeight(double objectHeight){
+        this.objectHeight=objectHeight;
+    }
+    @Override
+    public  void setObjectWidth(double objectWidth){
+        this.objectWidth=objectWidth;
+    }
+    @Override
+    public double getObjectWidth(){
+        return objectWidth;
+    }
+    @Override
+    public double getObjectHeight(){
+        return objectHeight;
     }
 
-    @Override
-    public double getObjectDimension() {
-        return this.objectDimension;
-    }
+    // @Override
+    // public double getObjectDimension() {
+    //     return this.objectDimension;
+    // }
 
-    @Override
-    public void setObjectDimension(double objectDimension) {
-        this.objectDimension = objectDimension;
-    }
+    // @Override
+    // public void setObjectDimension(double objectDimension) {
+    //     this.objectDimension = objectDimension;
+    // }
 }
